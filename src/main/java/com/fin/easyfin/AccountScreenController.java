c786b0a1aa51daeb91614242c574317d56518c60
Jonathan Ly <jly02@cs.washington.edu>
2022-08-05 00:07:32 -0700

    Update AccountScreenController

--- src/main/java/com/fin/easyfin/AccountScreenController.java
+++ src/main/java/com/fin/easyfin/AccountScreenController.java
@@ -1,8 +1,12 @@
 package com.fin.easyfin;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 /**
  * Handles all actions taking place on the account screen,
  * IE logging in and testing connections.
  */
 public class AccountScreenController {
+    @FXML public TextField usernameField;
 }
